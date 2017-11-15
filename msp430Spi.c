f93d136fe276511ff8c0de4fe29d4fdbcbff4b72
Sengming <nitricrage@gmail.com>
2017-11-15 11:31:54 -0500

    Currently works with kfifo to input and output bytes to application level program

--- msp430Spi.c
+++ msp430Spi.c
@@ -8,13 +8,20 @@
 #include <linux/cdev.h>
 #include <linux/slab.h>
 #include <linux/mm.h>
+#include <linux/ioport.h>
+#include <linux/errno.h>
+#include <linux/kfifo.h>
 
 // #defines:
 #define DEVICE_NAME ("msp430Spi")
 #define FIRST_MINOR_NUMBER (0)
 #define TOTAL_MINOR_NUMBERS (1)
-#define DEVICE_IN_BUFFER_SIZE (50)
-#define DEVICE_OUT_BUFFER_SIZE (50)
+#define DEVICE_IN_BUFFER_SIZE (128)
+#define DEVICE_OUT_BUFFER_SIZE (128)
+#define BCM2836_PERIPHERAL_BASE (0x3F000000)
+#define BCM2836_GPIO_BASE (BCM2836_PERIPHERAL_BASE + 0x200000)
+#define BCM2836_SPI_BASE (BCM2836_PERIPHERAL_BASE + 0x204000)
+#define BCM2836_SPI_LEN (0x18)
 
 // IRQ Handlers:
 irq_handler_t testHandler(int irqNumber, void* deviceId)
@@ -32,27 +39,47 @@ int g_deviceMajorNumber;
 // The device memory:
 struct msp430Spi
 {
-    char out_buffer[DEVICE_OUT_BUFFER_SIZE];
-    char in_buffer[DEVICE_IN_BUFFER_SIZE];
+    struct kfifo in_fifo;
+    struct kfifo out_fifo;
     struct semaphore deviceSem;
 };
 
 struct msp430Spi* p_msp430Spi;
 
 
-int device_open(struct inode* deviceFile, struct file* file)
+static void driver_cleanup(void)
+{
+    // Cleaning up:
+    unregister_chrdev_region(g_deviceNumber, TOTAL_MINOR_NUMBERS);
+    cdev_del(p_device);
+    kfifo_free(&p_msp430Spi->in_fifo);
+    kfifo_free(&p_msp430Spi->out_fifo);
+    kfree(p_msp430Spi);
+    release_mem_region(BCM2836_SPI_BASE, BCM2836_SPI_LEN);
+}
+
+static int device_open(struct inode* deviceFile, struct file* file)
 {
     int retVal = 0;
 
     // Sem Pend
     if(down_interruptible(&p_msp430Spi->deviceSem))
         retVal = -ERESTARTSYS;
+    // TODO: Remove, just used to test driver and populate fifo
+    unsigned char data0 = 255;
+    unsigned char data1 = 128;
+    unsigned char data2 = 0;
+    unsigned char data3 = 136;
 
+    kfifo_put(&p_msp430Spi->in_fifo, data0);
+    kfifo_put(&p_msp430Spi->in_fifo, data1);
+    kfifo_put(&p_msp430Spi->in_fifo, data2);
+    kfifo_put(&p_msp430Spi->in_fifo, data3);
     printk(KERN_INFO "Device Open called\n");
     return retVal;
 }
 
-int device_release(struct inode* deviceInode, struct file* deviceFileInstance)
+static int device_release(struct inode* deviceInode, struct file* deviceFileInstance)
 {
     int retVal = 0;
    
@@ -63,18 +90,43 @@ int device_release(struct inode* deviceInode, struct file* deviceFileInstance)
     return retVal;
 }
 
-int device_write(struct file* deviceFileInstance, const char __user* userBuffer, 
+static int device_write(struct file* deviceFileInstance, 
+        const char __user* userBuffer, 
         size_t size, loff_t* fileOffset)
 {
+    unsigned int numCopied = 0;
+    int retVal = 0;
     printk(KERN_INFO "Device write called\n");
-    return 0;
+    
+    retVal = kfifo_from_user(&p_msp430Spi->out_fifo, userBuffer, size, &numCopied);
+    if (0 > retVal)
+        goto out_copy_failed;
+
+    return numCopied;
+
+out_copy_failed:
+    driver_cleanup();
+    return retVal;
 }
 
-int device_read(struct file* deviceFileInstance, char __user* userBuffer, 
+static int device_read(struct file* deviceFileInstance, char __user* userBuffer, 
         size_t size, loff_t* fileOffset)
 {
+    unsigned int numCopied = 0;
+    int retVal = 0;
     printk(KERN_INFO "Device read called\n");
-    return 0;
+    
+    retVal =  kfifo_to_user(&p_msp430Spi->in_fifo, userBuffer, size, &numCopied);
+
+    if (0 > retVal)
+        goto out_copy_failed;
+    
+    // If all succeeds:
+    return numCopied;
+
+out_copy_failed:
+    driver_cleanup();
+    return retVal;
 }
 
 struct file_operations fops=
@@ -93,6 +145,15 @@ static int driver_entry(void)
 //	    const char *name, void *dev)
     int retVal = 0;
     p_msp430Spi = kmalloc(sizeof(struct msp430Spi), GFP_KERNEL);
+    if (p_msp430Spi == NULL)
+        goto out_major_number_bad;
+    retVal = kfifo_alloc(&(p_msp430Spi->in_fifo), DEVICE_IN_BUFFER_SIZE, GFP_KERNEL);
+    if (retVal)
+        goto out_fifo_bad;
+    retVal = kfifo_alloc (&(p_msp430Spi->out_fifo), DEVICE_OUT_BUFFER_SIZE, GFP_KERNEL);
+    if (retVal)
+        goto out_fifo_bad;
+
     printk(KERN_INFO "Driver Entry called!\n");
 
     sema_init(&p_msp430Spi->deviceSem,1);  
@@ -102,6 +163,7 @@ static int driver_entry(void)
     if (retVal < 0)
         goto out_major_number_bad;
 
+    // Allocate cdev and add to the system
     g_deviceMajorNumber = MAJOR(g_deviceNumber);
     p_device = cdev_alloc(); 
     p_device->owner = THIS_MODULE;
@@ -111,28 +173,39 @@ static int driver_entry(void)
     if (retVal < 0)
         goto out_cdev_bad;
 
+    // Attempt to grab control of memory mapped SPI IO:
+    if (NULL==request_mem_region(BCM2836_SPI_BASE, BCM2836_SPI_LEN, DEVICE_NAME))
+    {
+        goto out_iomem_bad;
+    }
+
     // Return 0 if all is good:
     return 0; 
 
-out_major_number_bad:
-    printk(KERN_ALERT "Failure to allocate major number!\n");
-    return retVal;
+out_iomem_bad:
+    printk(KERN_ALERT "IO memory could not be allocated!\n");
+
 out_cdev_bad:
     printk(KERN_ALERT "Failure to add cdev\n");
-    return retVal;
+    unregister_chrdev_region(g_deviceNumber, TOTAL_MINOR_NUMBERS);
+    
+out_fifo_bad:
+    printk(KERN_ALERT "Failure to allocate fifo!\n");
+    kfifo_free(&p_msp430Spi->in_fifo);
+    kfifo_free(&p_msp430Spi->out_fifo);
+
+out_major_number_bad:
+    printk(KERN_ALERT "Failure to allocate major number!\n");
+    kfree(p_msp430Spi); 
+
+    return retVal; 
 }
 
 static void driver_exit (void)
 {
     printk(KERN_INFO "Driver Exit called\n");
-    
-    // Cleaning up:
-    unregister_chrdev_region(g_deviceNumber, TOTAL_MINOR_NUMBERS);
-    cdev_del(p_device);
-    kfree(p_msp430Spi); 
+    driver_cleanup();    
 }
-
-
 
 module_init(driver_entry);
 module_exit(driver_exit);
